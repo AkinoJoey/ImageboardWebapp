73eaa335488080206d2637f98ae8919ffc921650
AkinoJoey <gakiya213@gmail.com>
2023-12-11 17:56:05 +0900

    postとコメントの作成途中

--- Views/component/top.php
+++ Views/component/top.php
@@ -1,1 +1,44 @@
-<p>test</p>
+<div class="container mx-auto px-4 my-10">
+    <!-- post card -->
+    <div class='flex items-center justify-center mt-8'>
+        <div class="rounded-xl border p-5 shadow-md bg-white w-1/2">
+            <!-- <div class="mt-4 mb-6">
+                <div class="mb-3 text-xl font-bold">Nullas neque neque neque?</div>
+                <div class="text-sm text-neutral-600">rhoncus neque nNulla sed leo tempus, feugiat velit vel, rhoncus neque nNulla sed leo tempus, feugiat velit vel, rhoncus neque nNulla sed leo tempus, feugiat velit vel, rhoncus neque nNulla sed leo tempus, feugiat velit vel, rhoncus neque n</div>
+            </div> -->
+
+            <div class="flex items-center justify-between text-slate-500">
+                <div class="flex space-x-4 md:space-x-8">
+                    <div class="flex cursor-pointer items-center transition hover:text-slate-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="mr-1.5 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                        </svg>
+                        <span>125</span>
+                    </div>
+                    <div class="flex cursor-pointer items-center transition hover:text-slate-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="mr-1.5 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
+                        </svg>
+                        <span>4</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- comments -->
+    <section class="flex items-center justify-center mt-2">
+        <div class="rounded-xl border p-5 shadow-md bg-white max-w-2xl ">
+            <article class="p-2 mb-1 text-base bg-white">
+                <footer class="flex justify-between items-center mb-2">
+                    <div class="flex items-center">
+                        <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold"><img class="mr-2 w-6 h-6 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Bonnie Green">Bonnie Green</p>
+                        <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-03-12" title="March 12th, 2022">Mar. 12, 2022</time></p>
+                    </div>
+                </footer>
+                <p class="text-gray-500 dark:text-gray-400">The articlegn strategy.The articlegn strategy.</p>
+            </article>
+        </div>
+    </section>
+
+</div>
