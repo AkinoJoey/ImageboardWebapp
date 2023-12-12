2c4c0a521437d40b412dd0dde091c2103527df2c
AkinoJoey <gakiya213@gmail.com>
2023-12-12 17:52:17 +0900

    フロントエンドをTachyonに変更

--- Views/component/top.php
+++ Views/component/top.php
@@ -1,44 +1,68 @@
-<div class="container mx-auto px-4 my-10">
-    <!-- post card -->
-    <div class='flex items-center justify-center mt-8'>
-        <div class="rounded-xl border p-5 shadow-md bg-white w-1/2">
-            <!-- <div class="mt-4 mb-6">
-                <div class="mb-3 text-xl font-bold">Nullas neque neque neque?</div>
-                <div class="text-sm text-neutral-600">rhoncus neque nNulla sed leo tempus, feugiat velit vel, rhoncus neque nNulla sed leo tempus, feugiat velit vel, rhoncus neque nNulla sed leo tempus, feugiat velit vel, rhoncus neque nNulla sed leo tempus, feugiat velit vel, rhoncus neque n</div>
-            </div> -->
-
-            <div class="flex items-center justify-between text-slate-500">
-                <div class="flex space-x-4 md:space-x-8">
-                    <div class="flex cursor-pointer items-center transition hover:text-slate-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="mr-1.5 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-                        </svg>
-                        <span>125</span>
-                    </div>
-                    <div class="flex cursor-pointer items-center transition hover:text-slate-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="mr-1.5 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
-                        </svg>
-                        <span>4</span>
-                    </div>
-                </div>
-            </div>
+<div class="flex flex-column items-center pt5 pt6-ns mb4">
+    <!-- post -->
+    <div class="w-100 w-50-ns ba flex items-center mt4">
+        <div class="flex flex-row items-center w-100 h3 p2">
+            <img class="w2 h2 w3-ns h3-ns br-100" src="/images/post.svg" />
+            <input class="input-reset ba b--black-20 pa2 db w-100 mr2" type="text" placeholder="今日何があった？">
         </div>
     </div>
 
-    <!-- comments -->
-    <section class="flex items-center justify-center mt-2">
-        <div class="rounded-xl border p-5 shadow-md bg-white max-w-2xl ">
-            <article class="p-2 mb-1 text-base bg-white">
-                <footer class="flex justify-between items-center mb-2">
-                    <div class="flex items-center">
-                        <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold"><img class="mr-2 w-6 h-6 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Bonnie Green">Bonnie Green</p>
-                        <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-03-12" title="March 12th, 2022">Mar. 12, 2022</time></p>
+    <!-- threads -->
+    <div>
+        <!-- post -->
+        <div class="flex items-center flex-column mt4">
+            <article class="flex flex-column hidden ba mh3 w-100 w-50-ns">
+                <h1 class="f4 bg-near-black white mv0 pv2 ph3">今日のご飯</h1>
+                <div class="pa3">
+                    <p class="f6 f5-ns lh-copy">
+                        みかん1個
+                    </p>
+                    <div class="tc">
+                        <!-- <img src="/images/tangerine.png"> -->
                     </div>
-                </footer>
-                <p class="text-gray-500 dark:text-gray-400">The articlegn strategy.The articlegn strategy.</p>
+                </div>
+            </article>
+
+            <!-- comments -->
+            <article class="flex flex-column hidden ba mt1 mh3 w-100 w-50-ns">
+                <h1 class="f4  mv0 pv2 ph3">Comments</h1>
+                <div class="flex items-center flex-column">
+                    <ul class="list pt0 pa3">
+                        <li class="flex items-center lh-copy ph0-l pv2 bt b--black-10">
+                            <img class="w2 h2 w3-ns h3-ns br-100" src="/images/user_icon.svg" />
+                            <div class="pl3 flex-auto">
+                                <span class="f6 db black-70">Medium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLC</span>
+                            </div>
+                        </li>
+                        <li class="flex items-center lh-copy ph0-l pv2 bt b--black-10">
+                            <img class="w2 h2 w3-ns h3-ns br-100" src="/images/user_icon.svg" />
+                            <div class="pl3 flex-auto">
+                                <span class="f6 db black-70"> みかんワロタ</span>
+                            </div>
+                        </li>
+                        <li class="flex items-center lh-copy ph0-l pv2 bt b--black-10">
+                            <img class="w2 h2 w3-ns h3-ns br-100" src="/images/user_icon.svg" />
+                            <div class="pl3 flex-auto">
+                                <span class="f6 db black-70"> LLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLC</span>
+                            </div>
+                        </li>
+                        <li class="flex items-center lh-copy ph0-l pv2 bt b--black-10">
+                            <img class="w2 h2 w3-ns h3-ns br-100" src="/images/user_icon.svg" />
+                            <div class="pl3 flex-auto">
+                                <span class="f6 db black-70"> LLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLLLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCLLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCLLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCLLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCLLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCLLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCLLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCLLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCLLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCLLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLCC</span>
+                            </div>
+                        </li>
+                        <li class="flex items-center lh-copy ph0-l pv2 bt b--black-10">
+                            <img class="w2 h2 w3-ns h3-ns br-100" src="/images/user_icon.svg" />
+                            <div class="pl3 flex-auto">
+                                <span class="f6 db black-70"> LLCMedium Hexagon, LLCMedium Hexagon, LLCMedium Hexagon, LLC</span>
+                            </div>
+                        </li>
+                    </ul>
+
+                </div>
             </article>
         </div>
-    </section>
+    </div>
 
 </div>
--- Views/layout/header.php
+++ Views/layout/header.php
@@ -4,21 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css" />
     <link rel="icon" href="/images/favicon.ico" sizes="any">
     <title>Kotatsu</title>
 </head>
 
 
 <body>
-    <header class="bg-white dark:bg-gray-900 sticky w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="/images/tangerine.png" class="h-8" alt="Kotatsu Logo">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Kotatsu</span>
-            </a>
-            <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">新しい話題 + </button>
+    <header class="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l ">
+        <nav class="f6 fw6 tracked flex justify-between items-center">
+            <div>
+                <a href=""><img src="/images/tangerine.png" class="v-mid pb1" width="30px" alt="Kotatsu logo"></a>
+                <a class="link dim white dib mr3" href="">Kotatsu</a>
             </div>
-        </div>
+            <div>
+                <a class="f6 link dim br1 ba bw2 ph3 pv2 dib white" href="#0">新しい話題 +</a>
+            </div>
+        </nav>
     </header>
