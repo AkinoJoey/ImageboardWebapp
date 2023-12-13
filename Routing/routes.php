4f10ffa5f19a161903868ade1b6dfa79ff37e047
AkinoJoey <gakiya213@gmail.com>
2023-12-13 19:36:13 +0900

    submitを追加

--- Routing/routes.php
+++ Routing/routes.php
@@ -5,5 +5,8 @@ use Response\Render\HTMLRenderer;
 return [
     '' => function () : HTMLRenderer {
         return new HTMLRenderer('component/top');
+    },
+    'submit' => function () : HTMLRenderer {
+        return new HTMLRenderer('component/submit');
     }
 ];
