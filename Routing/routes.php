322c6c1b2689bd3ccbaf4e00ec23e3ebfab43e4d
AkinoJoey <gakiya213@gmail.com>
2023-12-14 17:51:01 +0900

    form/threadを追加

--- Routing/routes.php
+++ Routing/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Response\Render\HTMLRenderer;
+use Response\Render\JSONRenderer;
 
 return [
     '' => function () : HTMLRenderer {
@@ -8,5 +9,12 @@ return [
     },
     'submit' => function () : HTMLRenderer {
         return new HTMLRenderer('component/submit');
+    },
+    'form/thread' => function () : JSONRenderer {
+        // validation
+        $title = $_POST['title'];
+        $body = $_POST['body'];
+
+        return new JSONRenderer(['success'=>true, 'message'=>$title]);
     }
 ];
