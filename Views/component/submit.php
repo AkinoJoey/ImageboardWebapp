2029c4bf774d643a427a83948f50757a784a9dc7
AkinoJoey <gakiya213@gmail.com>
2023-12-14 17:51:10 +0900

    formの値を修正

--- Views/component/submit.php
+++ Views/component/submit.php
@@ -1,13 +1,15 @@
 <h4 class="mt-3 mb-min">話題を投稿する</h4>
 <hr>
 <article class="mt-1">
-    <form method="POST">
+    <form id="thread-form" method="POST">
         <input type="text" id="title" name="title" placeholder="タイトル">
-        <textarea type="textarea" id="body" name="body" placeholder="内容" required style="height: 150px"></textarea>
-        <img class="image-icon" id="image-icon" src="/images/pic.svg" alt="">
-        <input class="d-none" type="file" id="image-input" name="image-input" accept="image/png, image/jpeg , image/gif">
-        <div style="height: 15px;" id="image-preview"></div>
+        <textarea type="textarea" id="body" name="body" placeholder="内容" required></textarea>
+        <label for="image-input">画像を選択：</label>
+        <input type="file" id="image-input" name="image-input" accept="image/png, image/jpeg , image/gif">
+        <div class="row end-xs mh-0">
+            <button class="mb-0 mw-6" type="submit">作成</button>
+        </div>
     </form>
 </article>
 
-<script src="/js/submit.js" ></script>
+<script src="/js/submit.js"></script>
