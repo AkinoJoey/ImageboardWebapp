e23c840ee4b5efe7accb251389015e12c14bc8a4
AkinoJoey <gakiya213@gmail.com>
2023-12-15 17:56:44 +0900

    スキームマイグレーションを追加

---
+++ Database/migrations/2023-12-15_1702622921_ChangePostTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class ChangePostTable implements SchemaMigration
+{
+    public function up(): array
+    {
+        return ["ALTER TABLE posts ADD COLUMN image_path VARCHAR(255) AFTER content;"];
+    }
+
+    public function down(): array
+    {
+        return ["ALTER TABLE posts posts DROP COLUMN image_path;"];
+    }
+}
