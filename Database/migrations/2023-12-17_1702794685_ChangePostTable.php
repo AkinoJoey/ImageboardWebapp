f7aeb23b7c28ff6cff06de5ee602ffb8f82b107c
AkinoJoey <gakiya213@gmail.com>
2023-12-17 16:13:39 +0900

    外部キー制約を追加

---
+++ Database/migrations/2023-12-17_1702794685_ChangePostTable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class ChangePostTable implements SchemaMigration
+{
+    public function up(): array
+    {
+        return [
+            "ALTER TABLE posts
+                ADD COLUMN main_thread_id INT,
+                ADD CONSTRAINT fk_main_thread
+                FOREIGN KEY (reply_to_id) REFERENCES posts(id) ON DELETE CASCADE;"
+        ];
+    }
+
+    public function down(): array
+    {
+        return [
+            "ALTER TABLE posts
+                DROP FOREIGN KEY fk_main_thread,
+                DROP COLUMN main_thread_id;"
+        ];
+    }
+}
