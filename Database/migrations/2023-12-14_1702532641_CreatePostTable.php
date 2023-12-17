b691bcc470876cc43330e492bff3190b37f30bc6
AkinoJoey <gakiya213@gmail.com>
2023-12-17 16:13:21 +0900

    語字を修正

--- Database/migrations/2023-12-14_1702532641_CreatePostTable.php
+++ Database/migrations/2023-12-14_1702532641_CreatePostTable.php
@@ -14,7 +14,7 @@ class CreatePostTable implements SchemaMigration
                     subject VARCHAR(50),
                     content TEXT,
                     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                    updated_at DATETIME NOT nULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);"];
+                    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);"];
     }
 
     public function down(): array
