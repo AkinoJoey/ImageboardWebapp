9ab5f0fb15a6f329a8bbd7896add3f01c857cc45
AkinoJoey <gakiya213@gmail.com>
2023-12-18 15:59:44 +0900

    requireとタイムゾーンセットを追加

--- Public/index.php
+++ Public/index.php
@@ -3,6 +3,11 @@ set_include_path(get_include_path() . PATH_SEPARATOR . realpath(__DIR__ . '/..')
 spl_autoload_extensions(".php");
 spl_autoload_register();
 
+require 'vendor/autoload.php';
+
+date_default_timezone_set('Asia/Tokyo');
+
+
 if (preg_match('/\.(?:png|jpg|jpeg|gif|js|css|html)$/', $_SERVER["REQUEST_URI"])) {
     return false;
 }
