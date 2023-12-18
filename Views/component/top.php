58201223be7c540bb2e9175c326424390ccda626
AkinoJoey <gakiya213@gmail.com>
2023-12-18 16:01:16 +0900

    サーバーサイドレンダリングの部分を追加

--- Views/component/top.php
+++ Views/component/top.php
@@ -1,9 +1,72 @@
+<?php 
+
+use Carbon\Carbon;
+
+?>
+
 <!-- post -->
 <article class="mt-3">
     <a href="/submit"><input type="text" placeholder="今日何かあった？"></a>
 </article>
 
 <!-- thread -->
+<?php foreach ($posts as $post) : ?>
+    <a class="text-decoration-none" href="<?= $post->getUrl() ?>">
+        <article class="thread">
+            <hgroup>
+                <div>
+                    <img src="/images/user_icon.svg" width="40px" alt="">
+                    <sup class="black" ><?= 'Posted by ' . htmlspecialchars(Carbon::parse($post->getTimeStamp()->getCreatedAt())->diffForHumans()) ?></sup>
+
+                    <?php if (!is_null($post->getSubject())) : ?>
+                        <h2 class="mb-0"><?= htmlspecialchars($post->getSubject()) ?></h2>
+                    <?php endif; ?>
+                </div>
+                <p><?= htmlspecialchars($post->getContent()) ?></p>
+                <?php if (!is_null($post->getImagePath())) : ?>
+                    <div class="row center-xs">
+                        <img class="w-35" src="<?= '/uploads/' . htmlspecialchars($post->getImagePath())  ?>" alt="">
+                    </div>
+                <?php endif; ?>
+
+            </hgroup>
+            <a href="<?= $post->getUrl() ?>"><img src="/images/comment.svg" alt="" title="返信"></a>
+
+            <footer>
+                <h2>Comments</h2>
+                <?php foreach ($allComment as $comments) : ?>
+                    <?php foreach ($comments as $comment) : ?>
+                        <?php if ($post->getId() == $comment->getReplyToId()) : ?>
+                            <div class="mb-3">
+                                <div class="row mh-0">
+                                    <img src="/images/user_icon.svg" alt="user icon">
+                                    <small><?= htmlspecialchars(Carbon::parse($comment->getTimeStamp()->getCreatedAt())->diffForHumans()) ?></small>
+                                    <h4 class="mb-0 ml-4"><?= $comment->getSubject() ?></h4>
+                                </div>
+                                <p><?= $comment->getContent() ?></p>
+                                <!-- img -->
+                                <?php if (!is_null($comment->getImagePath())) : ?>
+                                    <div class="row center-xs">
+                                        <img class="w-35" src="<?= '/uploads/' . htmlspecialchars($comment->getImagePath()) ?>" alt="uploaded image">
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        <?php endif; ?>
+                    <?php endforeach ?>
+                <?php endforeach; ?>
+
+                <hr>
+                <a href="<?= $post->getUrl() ?>">
+                    <p class="text-align-center mb-0 mt-1">すべてのコメント</p>
+                </a>
+
+            </footer>
+        </article>
+    </a>
+
+<?php endforeach; ?>
+
+
 <article class="thread">
     <hgroup>
         <div class="">
