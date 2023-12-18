88c624ab3909d0c6785699c9c1ea773f6171ed67
AkinoJoey <gakiya213@gmail.com>
2023-12-18 17:52:06 +0900

    不要なデータを削除

--- Views/component/top.php
+++ Views/component/top.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 use Carbon\Carbon;
 
@@ -11,110 +11,59 @@ use Carbon\Carbon;
 
 <!-- thread -->
 <?php foreach ($posts as $post) : ?>
-    <a class="text-decoration-none" href="<?= $post->getUrl() ?>">
-        <article class="thread">
-            <hgroup>
-                <div>
-                    <img src="/images/user_icon.svg" width="40px" alt="">
-                    <sup class="black" ><?= 'Posted by ' . htmlspecialchars(Carbon::parse($post->getTimeStamp()->getCreatedAt())->diffForHumans()) ?></sup>
+    <article class="thread">
+        <a class="expandLink" href="<?= $post->getUrl() ?>"></a>
+        <hgroup>
+            <div>
+                <img src="/images/user_icon.svg" width="40px" alt="">
+                <sup class="black"><?= 'Posted by ' . htmlspecialchars(Carbon::parse($post->getTimeStamp()->getCreatedAt())->diffForHumans()) ?></sup>
 
-                    <?php if (!is_null($post->getSubject())) : ?>
-                        <h2 class="mb-0"><?= htmlspecialchars($post->getSubject()) ?></h2>
-                    <?php endif; ?>
-                </div>
-                <p><?= htmlspecialchars($post->getContent()) ?></p>
-                <?php if (!is_null($post->getImagePath())) : ?>
-                    <div class="row center-xs">
-                        <img class="w-35" src="<?= '/uploads/' . htmlspecialchars($post->getImagePath())  ?>" alt="">
-                    </div>
+                <?php if (!is_null($post->getSubject())) : ?>
+                    <h2 class="mb-0"><?= htmlspecialchars($post->getSubject()) ?></h2>
                 <?php endif; ?>
+            </div>
+            <p><?= htmlspecialchars($post->getContent()) ?></p>
+            <?php if (!is_null($post->getImagePath())) : ?>
+                <div class="row center-xs">
+                    <img class="w-35" src="<?= '/uploads/' . htmlspecialchars($post->getImagePath())  ?>" alt="">
+                </div>
+            <?php endif; ?>
 
-            </hgroup>
-            <a href="<?= $post->getUrl() ?>"><img src="/images/comment.svg" alt="" title="返信"></a>
+        </hgroup>
+        <img src="/images/comment.svg" alt="" title="返信">
 
-            <footer>
-                <h2>Comments</h2>
-                <?php foreach ($allComment as $comments) : ?>
-                    <?php foreach ($comments as $comment) : ?>
-                        <?php if ($post->getId() == $comment->getReplyToId()) : ?>
-                            <div class="mb-3">
-                                <div class="row mh-0">
-                                    <img src="/images/user_icon.svg" alt="user icon">
-                                    <small><?= htmlspecialchars(Carbon::parse($comment->getTimeStamp()->getCreatedAt())->diffForHumans()) ?></small>
-                                    <h4 class="mb-0 ml-4"><?= $comment->getSubject() ?></h4>
-                                </div>
-                                <p><?= $comment->getContent() ?></p>
-                                <!-- img -->
-                                <?php if (!is_null($comment->getImagePath())) : ?>
-                                    <div class="row center-xs">
-                                        <img class="w-35" src="<?= '/uploads/' . htmlspecialchars($comment->getImagePath()) ?>" alt="uploaded image">
-                                    </div>
-                                <?php endif; ?>
+        <footer>
+            <h2>Comments</h2>
+            <?php foreach ($allComment as $comments) : ?>
+                <?php foreach ($comments['comments'] as $comment) : ?>
+                    <?php if ($post->getId() == $comment->getReplyToId()) : ?>
+                        <div class="mb-3">
+                            <div class="row mh-0">
+                                <img src="/images/user_icon.svg" alt="user icon">
+                                <small><?= htmlspecialchars(Carbon::parse($comment->getTimeStamp()->getCreatedAt())->diffForHumans()) ?></small>
+                                <h4 class="mb-0 ml-4"><?= $comment->getSubject() ?></h4>
                             </div>
-                        <?php endif; ?>
-                    <?php endforeach ?>
-                <?php endforeach; ?>
+                            <p><?= $comment->getContent() ?></p>
+                            <!-- img -->
+                            <?php if (!is_null($comment->getImagePath())) : ?>
+                                <div class="row center-xs">
+                                    <img class="w-35" src="<?= '/uploads/' . htmlspecialchars($comment->getImagePath()) ?>" alt="uploaded image">
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    <?php endif; ?>
+                <?php endforeach ?>
 
-                <hr>
-                <a href="<?= $post->getUrl() ?>">
-                    <p class="text-align-center mb-0 mt-1">すべてのコメント</p>
-                </a>
+                <?php if ($comments['hasMoreComments']) : ?>
+                        <hr>
+                        <a>
+                            <p class="text-align-center mb-0 mt-1">すべてのコメント</p>
+                        </a>
+                    <?php endif; ?>
 
-            </footer>
-        </article>
-    </a>
+            <?php endforeach; ?>
+
+        </footer>
+    </article>
 
 <?php endforeach; ?>
-
-
-<article class="thread">
-    <hgroup>
-        <div class="">
-            <img src="/images/user_icon.svg" width="40px" alt="">
-            <h2 class="mb-0">title</h2>
-        </div>
-        <p>body texbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textbody textt</p>
-        <div class="row center-xs">
-            <img class="w-35" src="/images/4.5MB.jpg" alt="">
-        </div>
-    </hgroup>
-    <a href="#"><img src="/images/comment.svg" alt="" title="返信"></a>
-
-    <footer>
-        <h2>Comments</h2>
-        <div class="mb-3">
-            <div class="row mh-0">
-                <img src="/images/user_icon.svg" alt="">
-                <h4 class="mb-0 ml-4">title</h4>
-            </div>
-            <p>Go to Safari > Settings > Websites > Notifications, and remove or change all sites to not allow Notifications.</p>
-        </div>
-        <div class="mb-3">
-            <div class="row mh-0">
-                <img src="/images/user_icon.svg" alt="">
-                <h4 class="mb-0 ml-4">title</h4>
-            </div>
-            <p>犬ワロタ</p>
-        </div>
-        <div class="mb-3">
-            <div class="row mh-0">
-                <img src="/images/user_icon.svg" alt="">
-                <h4 class="mb-0 ml-4">title</h4>
-            </div>
-            <p>Go to Safari > Settings > Websites > Notifications, and remove or change all sites to not allow Notifications.</p>
-        </div>
-        <div class="mb-3">
-            <div class="row mh-0">
-                <img src="/images/user_icon.svg" alt="">
-                <h4 class="mb-0 ml-4">title</h4>
-            </div>
-            <p>Go to Safari > Settings > Websites > Notifications, and remove or change all sites to not allow Notifications.</p>
-        </div>
-
-        <hr>
-        <a href="">
-            <p class="text-align-center mb-0 mt-1">すべてのコメント</p>
-        </a>
-
-    </footer>
-</article>
