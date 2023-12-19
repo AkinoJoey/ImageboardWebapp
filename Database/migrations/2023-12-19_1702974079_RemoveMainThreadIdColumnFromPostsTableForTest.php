96db7949ecb5c8c86866043f4eae5a0613dd8815
AkinoJoey <gakiya213@gmail.com>
2023-12-19 17:50:37 +0900

    テストのために外部キー制約をドロップした

---
+++ Database/migrations/2023-12-19_1702974079_RemoveMainThreadIdColumnFromPostsTableForTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class RemoveMainThreadIdColumnFromPostsTableForTest implements SchemaMigration
+{
+
+    public function up(): array
+    {
+        return [
+            "ALTER TABLE posts
+                DROP FOREIGN KEY fk_main_thread,
+                DROP COLUMN main_thread_id;"
+        ];
+    }
+    
+    public function down(): array
+    {
+        return [
+            "ALTER TABLE posts
+                ADD COLUMN main_thread_id INT,
+                ADD CONSTRAINT fk_main_thread
+                FOREIGN KEY (reply_to_id) REFERENCES posts(id) ON DELETE CASCADE;"
+        ];
+    }
+
+    
+}
