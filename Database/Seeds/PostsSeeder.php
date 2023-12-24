4a8a99cd23ec3985b603e530ad202b99431c080b
AkinoJoey <gakiya213@gmail.com>
2023-12-24 10:04:49 +0900

    最終テスト用に使用

--- Database/Seeds/PostsSeeder.php
+++ Database/Seeds/PostsSeeder.php
@@ -52,7 +52,7 @@ class PostsSeeder extends AbstractSeeder{
         $min_year = strtotime('1990-01-01 00:00:00');
         $max_year = strtotime('2023-12-01 00:00:00');
 
-        for ($i = 0; $i < 1000; $i++) {
+        for ($i = 0; $i < 100; $i++) {
             $randomTimeStampCreated = mt_rand($min_year, $max_year);
             $randomTimeStampUpdated = mt_rand($randomTimeStampCreated, $max_year);
 
@@ -60,22 +60,22 @@ class PostsSeeder extends AbstractSeeder{
             $thumbnailPath = 'bf/bfc6a1d034457e10cda64b649409ebbf224c4261b2b445d3acb7e9300dddbde6_thumbnail.jpeg';
 
             // id 1から100までメインスレッド
-            // $row = [
-            //         null,
-            //         $faker->realTextBetween(1, 50, 5),
-            //         $faker->realTextBetween(160, 1000, 5),
-            //         $imagePath,
-            //         $thumbnailPath,
-            //         '/thread/' . $faker->slug(),
-            //         Carbon::createFromTimestamp($randomTimeStampCreated)->toDateTime(),
-            //         Carbon::createFromTimestamp($randomTimeStampUpdated)->toDateTime(),
-            // ];
+            $row = [
+                    null,
+                    $faker->realTextBetween(1, 50, 5),
+                    $faker->realTextBetween(160, 1000, 5),
+                    $imagePath,
+                    $thumbnailPath,
+                    '/thread/' . $faker->slug(),
+                    Carbon::createFromTimestamp($randomTimeStampCreated)->toDateTime(),
+                    Carbon::createFromTimestamp($randomTimeStampUpdated)->toDateTime(),
+            ];
 
             $imagePath = 'ab/ab31cded011645c46ded35b9db2a79f7250e7672522b6117cdb58592f498413e.png';
             $thumbnailPath = 'ab/ab31cded011645c46ded35b9db2a79f7250e7672522b6117cdb58592f498413e_thumbnail.png';
 
             $row = [
-                $faker->numberBetween(1, 1000), //reply to id
+                $faker->numberBetween(1, 100), //reply to id
                 $faker->realTextBetween(1, 20, 5),
                 $faker->realTextBetween(20, 140, 5),
                 $imagePath,
