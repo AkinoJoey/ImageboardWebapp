9080d41fffc682cdabc6a156a472bb6cbe95f437
AkinoJoey <gakiya213@gmail.com>
2023-12-31 19:01:28 +0900

    memcachedの関数を追加

--- Database/DatabaseManager.php
+++ Database/DatabaseManager.php
@@ -2,9 +2,13 @@
 
 namespace Database;
 
+use Helpers\Settings;
+use Memcached;
+
 class DatabaseManager
 {
     protected static array $mysqliConnections = [];
+    protected static array $memcachedConnections = [];
 
     public static function getMysqliConnection(string $connectionName = 'default'): MySQLWrapper
     {
@@ -13,5 +17,16 @@ class DatabaseManager
         }
 
         return static::$mysqliConnections[$connectionName];
+    }
+
+    public static function getMemcachedConnection(string $connectionName = 'default'): Memcached
+    {
+        if (!isset(static::$memcachedConnections[$connectionName])) {
+            $memcached = new Memcached();
+            $memcached->addServer(Settings::env('MEMCACHED_HOST'), Settings::env('MEMCACHED_PORT'));
+            static::$memcachedConnections[$connectionName] = $memcached;
+        }
+
+        return static::$memcachedConnections[$connectionName];
     }
 }
