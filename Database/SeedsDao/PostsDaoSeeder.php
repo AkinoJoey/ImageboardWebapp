a0d7085f8e728be257fb228c313e73af19afcd02
AkinoJoey <gakiya213@gmail.com>
2024-01-05 15:33:13 +0900

    タイムスタンプ部分を修正

--- Database/SeedsDao/PostsDaoSeeder.php
+++ Database/SeedsDao/PostsDaoSeeder.php
@@ -7,6 +7,8 @@ require_once 'vendor/autoload.php';
 use Database\DataAccess\DAOFactory;
 use Models\Post;
 use Faker\Factory;
+use Models\DataTimeStamp;
+use Carbon\Carbon;
 
 class PostsDaoSeeder{
 
@@ -19,23 +21,30 @@ class PostsDaoSeeder{
 
     public function createDummyPosts(): array{
         $faker = Factory::create();
-        $data = [];
+        $min_year = strtotime('1990-01-01 00:00:00');
+        $max_year = strtotime('2023-12-01 00:00:00');
         $defaultItemCount = 50;
+        $data = [];
 
         // スレッドのダミー
         for($i = 0; $i < $defaultItemCount; $i++){
+            $randomTimeStampCreated = mt_rand($min_year, $max_year);
+            $randomTimeStampUpdated = mt_rand($randomTimeStampCreated, $max_year);
+            $createdAt = Carbon::createFromTimestamp($randomTimeStampCreated)->toDateString();
+            $updatedAt = Carbon::createFromTimestamp($randomTimeStampUpdated)->toDateString();
+            $timestamp = new DataTimeStamp($createdAt, $updatedAt);
             $imagePath = null;
             $thumbnailPath = null;
 
             $thread = new Post(
-                $faker->realTextBetween(160, 1000, 5),
+                $faker->realTextBetween(10, 1000, 5),
                 $faker->realTextBetween(1, 50, 5),
                 '/thread/' . $faker->slug(),
                 null,
                 null,
                 $imagePath,
                 $thumbnailPath,
-                null
+                $timestamp
             );
 
             $data[] = $thread;
@@ -43,18 +52,23 @@ class PostsDaoSeeder{
 
         // コメントのダミー
         for ($i = 0; $i < $defaultItemCount; $i++) {
+            $randomTimeStampCreated = mt_rand($min_year, $max_year);
+            $randomTimeStampUpdated = mt_rand($randomTimeStampCreated, $max_year);
+            $createdAt = Carbon::createFromTimestamp($randomTimeStampCreated)->toDateString();
+            $updatedAt = Carbon::createFromTimestamp($randomTimeStampUpdated)->toDateString();
+            $timestamp = new DataTimeStamp($createdAt, $updatedAt);
             $imagePath = null;
             $thumbnailPath = null;
 
             $reply = new Post(
-                $faker->realTextBetween(20, 140, 5),
+                $faker->realTextBetween(10, 50, 5),
                 $faker->realTextBetween(1, 20, 5),
                 null,
-                $faker->numberBetween(0, 49),
+                $faker->numberBetween(1, 49),
                 null,
                 $imagePath,
                 $thumbnailPath,
-                null
+                $timestamp
             );
 
             $data[] = $reply;
